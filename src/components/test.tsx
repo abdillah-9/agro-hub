45a79d997f953613404f1d6bc9c6193fa1ac698c
abdillah-9 <sabdillah855@gmail.com>
2026-01-16 11:09:38 +0300

    shiit

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -18,6 +18,19 @@ export default function Test(){
             <div style={{boxShadow:'1px 1px 20px black', borderRadius:'10px',padding:'20px'}}>
                 chozo
             </div>
+
+            <div style={{
+  display: 'grid',
+  gridTemplateColumns: '1fr 2fr 1fr',
+  gridTemplateRows: '100px 200px',
+  gap: '10px',
+}}>
+  <div style={{ background: 'red' }}>1</div>
+  <div style={{ background: 'blue', gridRow: 'span 2' }}>2 (tall)</div>
+  <div style={{ background: 'green' }}>3</div>
+  <div style={{ background: 'orange' }}>4</div>
+</div>
+
         </div>
     )
 }
