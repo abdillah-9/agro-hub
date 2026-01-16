5e4280d09ddbc0ea5e6e10980ddbf633e1743fc1
abdillah-9 <sabdillah855@gmail.com>
2026-01-16 10:45:03 +0300

    shiit

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,14 +1,14 @@
 export default function Test(){
     const jaja = 'food';
     return(
-        <div style={{display:'grid'}}>
-            <div style={{boxShadow:'1px 1px 20px black', borderRadius:'10px'}}>
+        <div style={{backgroundColor:'white',width:'100%',display:'grid'}}>
+            <div style={{boxShadow:'1px 1px 20px black', borderRadius:'10px',padding:'20px'}}>
                 bonzo
             </div>
-            <div style={{boxShadow:'1px 1px 20px black', borderRadius:'10px'}}>
+            <div style={{boxShadow:'1px 1px 20px black', borderRadius:'10px',padding:'20px'}}>
                 ponzo
             </div>
-            <div style={{boxShadow:'1px 1px 20px black', borderRadius:'10px'}}>
+            <div style={{boxShadow:'1px 1px 20px black', borderRadius:'10px',padding:'20px'}}>
                 chozo
             </div>
         </div>
